185334dbb1016e915c9ae4c2f4dcc48e0bd3f9f7
bhargav_22 <mvsbari02@gmail.com>
2026-03-10 17:03:50 +0530

    Added models related to teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_10_104104) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_10_110211) do
   create_table "admin_users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -29,4 +29,22 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_10_104104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "team_players", force: :cascade do |t|
+    t.integer "team_id", null: false
+    t.integer "player_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_team_players_on_player_id"
+    t.index ["team_id"], name: "index_team_players_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "team_players", "players"
+  add_foreign_key "team_players", "teams"
 end
