34ddda7e0e710b350a1715535df14a2aa0799602
bhargav_22 <mvsbari02@gmail.com>
2026-03-11 00:38:57 +0530

    [7] Added status to Player

--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -1,4 +1,14 @@
 class TeamPlayer < ApplicationRecord
   belongs_to :team
   belongs_to :player
+
+  validate :player_must_be_active
+
+  private
+
+  def player_must_be_active
+    unless player.active?
+      errors.add(:player, "must be active to be added to a team")
+    end
+  end
 end
