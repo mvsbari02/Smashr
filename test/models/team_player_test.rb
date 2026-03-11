66600b944087ccf735b46462dab68000b3c04c66
bhargav_22 <mvsbari02@gmail.com>
2026-03-11 21:10:11 +0530

    removed test for activeness of player for now as I removed the same logic from the model for now

--- test/models/team_player_test.rb
+++ test/models/team_player_test.rb
@@ -1,19 +1,19 @@
 require "test_helper"
 
 class TeamPlayerTest < ActiveSupport::TestCase
-  test "player must be active to join a team" do
-    team = Team.create!(name: "Team A")
+  # test "player must be active to join a team" do
+  #   team = Team.create!(name: "Team A")
 
-    player = Player.create!(
-      name: "John",
-      email: "john@example.com",
-      country: "India",
-      status: :beta_active
-    )
+  #   player = Player.create!(
+  #     name: "John",
+  #     email: "john@example.com",
+  #     country: "India",
+  #     status: :beta_active
+  #   )
 
-    team_player = TeamPlayer.new(team: team, player: player)
+  #   team_player = TeamPlayer.new(team: team, player: player)
 
-    assert_not team_player.valid?
-    assert_includes team_player.errors[:player], "must be active to be added to a team"
+  #   assert_not team_player.valid?
+  #   assert_includes team_player.errors[:player], "must be active to be added to a team"
   end
 end
